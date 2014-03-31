7e8226d9b040b9e13b08a4b18092c9b2d83f216a
Afuna <fu@dreamwidth.org>
2014-03-31 18:52:37 +0800

    Record if a bug is a security issue
    We parse this information from the email body, and then save it as
    either a "Y" or "N"

--- bugzilla-email-recovery/extract.py
+++ bugzilla-email-recovery/extract.py
@@ -11,6 +11,7 @@ import hashlib
 url_regex = re.compile(r"http")
 whitespace_regex = re.compile(r"^\s*$")
 bugzilla_change_table_regex = re.compile(r".* <.*> changed:$")
+bugzilla_summary_header_regex = re.compile("\s+Summary:")
 horizontal_line_regex = re.compile(r"-------------------")
 comment_header_regex = re.compile(r"--- Comment #(\d+) from .+? ---$")
 footer_regex = re.compile(r"^-- $")
@@ -24,10 +25,12 @@ def parse_body(body):
     STATE_BUGZILLA_CHANGE_TABLE_HEADER = 3
     STATE_BUGZILLA_CHANGE_TABLE_ROW = 4
     STATE_BUGZILLA_COMMENT = 5
+    STATE_BUGZILLA_SUMMARY_HEADER = 6
 
     state = STATE_BEGIN
     output = []
     comment_id = 0
+    is_security = False
     for line in lines:
         output.append(line)
 
@@ -44,6 +47,8 @@ def parse_body(body):
             elif bugzilla_change_table_regex.match(line):
                 state = STATE_BUGZILLA_CHANGE_TABLE_HEADER
                 continue
+            elif bugzilla_summary_header_regex.match(line):
+                state = STATE_BUGZILLA_SUMMARY_HEADER
             else:
                 match = comment_header_regex.match(line)
                 if match:
@@ -55,6 +60,18 @@ def parse_body(body):
                 state = STATE_BUGZILLA_CHANGE_TABLE_ROW
             continue
 
+        if state == STATE_BUGZILLA_SUMMARY_HEADER:
+            if whitespace_regex.match(line):
+                state = STATE_BETWEEN_SECTIONS
+
+            # some values can be split across multiple lines. We ignore that for now
+            if ":" in line:
+                (key, value) = [x.strip() for x in line.split(":", 1)]
+
+                if key == "Group" and value == "Security Issue":
+                    is_security = True
+            continue
+
         if state == STATE_BUGZILLA_CHANGE_TABLE_ROW:
             if whitespace_regex.match(line):
                 state = STATE_BETWEEN_SECTIONS
@@ -63,6 +80,11 @@ def parse_body(body):
                 # if we want to detect anything further about bug state, we can do this here
                 # note: it looks like old emails have the changed table
                 # emails from 2012 onward have an additional set of information
+                (key, removed, added) = [x.strip() for x in line.split("|", 2)]
+                if key == "Group" and added == "Security Issue":
+                    is_security = True
+
+                continue
                 pass
 
         if state == STATE_BUGZILLA_COMMENT:
@@ -71,7 +93,7 @@ def parse_body(body):
         if footer_regex.match(line):
             break
 
-    return {"body": "\n".join(output), "comment_id": comment_id}
+    return {"body": "\n".join(output), "comment_id": comment_id, "is_security": is_security}
 
 
 def read_file(filename, conn):
@@ -109,6 +131,23 @@ def read_file(filename, conn):
             date_cleaned_text = date_regex.sub("", message["Date"])
             date_received = datetime.datetime.strptime(date_cleaned_text, "%a, %d %b %Y %H:%M:%S")
 
+            # parse additional information from the comment body
+            for part in message.walk():
+                content_type = part.get_content_type()
+                if content_type == "text/plain":
+                    parsed = parse_body(part.get_payload())
+                    add_comment_args = (matched_subject["bug_id"], parsed["comment_id"],
+                                        message["X-Bugzilla-Who"], parsed["body"],
+                                        date_received, hashlib.md5(parsed["body"]).hexdigest())
+                    cursor.execute("INSERT OR IGNORE INTO comments VALUES (?,?,?,?,?,?)", add_comment_args)
+                    is_security = parsed["is_security"]
+                else:
+                    # multipart/alternative, text/html
+                    # but we assume that content is available in text/plain so we ignore these
+                    #print "Got a message part of content type %s; not handling (%s: %s)" \
+                    #    % (content_type, filename, subject_line)
+                    continue
+
             cursor.execute("SELECT last_update from buginfo where bug_id = ?", (matched_subject["bug_id"], ))
             last_updated = cursor.fetchone()
 
@@ -121,9 +160,13 @@ def read_file(filename, conn):
                     update_bug_args.append(date_received)
                     update_bug_args.append(matched_subject["bug_id"])
 
-                    cursor.execute("UPDATE buginfo SET subject=?, product=?, component=?, severity=?," +
-                                   " priority=?, status=?, assigned=?, last_update=?" +
-                                   " WHERE bug_id = ?", update_bug_args)
+                    sql = ["UPDATE buginfo SET subject=?, product=?, component=?, severity=?,",
+                           "priority=?, status=?, assigned=?, last_update=?"]
+                    if is_security:
+                        sql.append(", is_security='Y'")
+                    sql.append("WHERE bug_id = ?")
+
+                    cursor.execute(" ".join(sql), update_bug_args)
                 # bug that exists, but we have newer information (possibly merged from two data sources): ignore
                 else:
                     pass
@@ -133,29 +176,13 @@ def read_file(filename, conn):
                     matched_subject["subject"],
                 ]
                 add_bug_args.extend([(message["X-Bugzilla-" + x]) for x in interesting_attributes])
+                if is_security:
+                    add_bug_args.append("Y")
+                else:
+                    add_bug_args.append("N")
                 add_bug_args.append(date_received)
 
-                cursor.execute("INSERT INTO buginfo VALUES (?,?,?,?,?,?,?,?,?)", add_bug_args)
-
-            for part in message.walk():
-                content_type = part.get_content_type()
-                if content_type == "text/plain":
-                    parsed = parse_body(part.get_payload())
-                    if parsed is None:
-                        # print "Not a bugzilla comment; not handling (%s: %s)" \
-                        #    % (filename, subject_line)
-                        pass
-                    else:
-                        add_comment_args = (matched_subject["bug_id"], parsed["comment_id"],
-                                            message["X-Bugzilla-Who"], parsed["body"],
-                                            date_received, hashlib.md5(parsed["body"]).hexdigest())
-                        cursor.execute("INSERT OR IGNORE INTO comments VALUES (?,?,?,?,?,?)", add_comment_args)
-                else:
-                    # multipart/alternative, text/html
-                    # but we assume that content is available in text/plain so we ignore these
-                    #print "Got a message part of content type %s; not handling (%s: %s)" \
-                    #    % (content_type, filename, subject_line)
-                    continue
+                cursor.execute("INSERT INTO buginfo VALUES (?,?,?,?,?,?,?,?,?,?)", add_bug_args)
 
         if counter % 100 == 0:
             conn.commit()
@@ -163,6 +190,7 @@ def read_file(filename, conn):
 
     conn.commit()
     sys.stderr.write(" ...%d\n" % counter)
+
 
 def get_connection():
     return sqlite3.connect("data.db")
